bccd1552299733a4535e627c0322796defb1ba25
morimoto kazuma <moricccccooooo2558@gmail.com>
2017-06-19 15:11:44 +0900

    標準クラス課題　日付編1

---
+++ CampChallenge_java/015.標準クラス/callenge2_1.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.mypackage;
+
+/**
+ *
+ * @author PCUser
+ */
+
+import java.util.Calendar;
+
+public class callenge2_1 {
+    
+    public static void main(String[] args){
+    
+    Calendar calendar = Calendar.getInstance();
+    
+    calendar.set(2016,1,1,0,0,0);
+    
+    System.out.print(calendar.getTime());
+    
+}
+}
