59de5c6bd8305d038cbbccacb0808dbacdd6edec
morimoto kazuma <moricccccooooo2558@gmail.com>
2017-06-23 11:28:40 +0900

    標準クラス　文字列課題1

---
+++ CampChallenge_java/015.標準クラス/challenge3_1.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.mypackage;
+
+/**
+ *
+ * @author PCUser
+ */
+public class challenge3_1 {
+    
+    public static void main(String[] args){
+    
+        String name = "森本佳寿馬";
+        
+        System.out.println(name.length());
+        System.out.println("森本佳寿馬".getBytes().length);
+        
+    }
+    
+}
