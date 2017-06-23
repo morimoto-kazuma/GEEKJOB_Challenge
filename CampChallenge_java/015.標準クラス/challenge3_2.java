bd3b2a5c19a95ec21fab3224ab7a1e625ea335eb
morimoto kazuma <moricccccooooo2558@gmail.com>
2017-06-23 11:29:55 +0900

    標準クラス　文字列課題2

---
+++ CampChallenge_java/015.標準クラス/challenge3_2.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.mypackage;
+
+/**
+ *
+ * @author PCUser
+ */
+public class challenge3_2 {
+    
+    public static void main(String[] args){
+    
+    String mail = "ka--zu.ma@docomo.ne.jp";
+    
+    System.out.println(mail.substring(10));
+    } 
+    
+}
