eeec3ee519a17368dae932b34e0df5c80299da83
morimoto kazuma <moricccccooooo2558@gmail.com>
2017-07-22 16:30:19 +0900

    条件分岐 課題 if文

---
+++ CampChallenge_Ruby/005.条件分岐/if.rb
@@ -0,0 +1,14 @@
+num1 = 1
+num2 = 2
+key = 1
+pattern1 = "1です"
+pattern2 = "プログラミングキャンプ！"
+pattern3 = "その他です！"
+
+if key == num1
+  puts pattern1
+elsif key == num2
+  puts pattern2
+else
+  puts pattern3
+end
