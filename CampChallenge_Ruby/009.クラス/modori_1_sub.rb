c4ad7216a28c53db51cfb6e72bcba211d1441045
morimoto kazuma <moricccccooooo2558@gmail.com>
2017-07-27 10:19:40 +0900

    クラス メソッド課題 戻り値1

---
+++ CampChallenge_Ruby/009.クラス/modori_1_sub.rb
@@ -0,0 +1,11 @@
+class Profile
+
+    def set_prof(name,birth,pro)
+        puts "名前：#{name}"
+        puts "生年月日：#{birth}"
+        puts "自己紹介：#{pro}"
+
+        return true
+
+        end
+       end
