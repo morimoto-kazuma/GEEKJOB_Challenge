eaee19dc8a848066068412425edbd61463d2a0a5
morimoto kazuma <moricccccooooo2558@gmail.com>
2017-07-27 10:52:00 +0900

    クラス メソッド課題 戻り値2

---
+++ CampChallenge_Ruby/009.クラス/modori_2.rb
@@ -0,0 +1,13 @@
+#1. 戻り値として、人物のID、名前、生年月日、住所を
+#配列にして返却するメソッドを作成してください。
+#2. 作成したメソッドを呼び出し、
+#戻り値のID以外を表示してください。
+
+class Back 
+    def profile(id,name,birth,address)
+
+       return prof = [id,name,birth,address]
+     
+        end
+
+    end
---
+++ CampChallenge_Ruby/009.クラス/modori_2_sub.rb
@@ -0,0 +1,6 @@
+require './modori_2.rb'
+back = Back.new
+hako = back.profile(1,"森本","09-09","大阪")
+ puts hako[1]
+ puts hako[2]
+ puts hako[3]
